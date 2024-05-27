2b00b0439c518690a6ee1c90cd07faa70cb38bf9
Ji-Han-Lee <rjsgml3747@naver.com>
2024-05-27 16:24:51 +0900

    받아오기

---
+++ myweb/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.conf import settings
+# Create your views here.
+def index(request):
+    return render('template/menu/index.html')
